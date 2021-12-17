b504ab2a111e41afc1233218b6269fe55f433cee
swag31415 <kidswagatam18@gmail.com>
2021-12-17 17:51:35 -0500

    Added all the stuff from swag_site and made it work with the MiscServer and gh-pages and all that

---
+++ js/main.js
@@ -0,0 +1,36 @@
+let advice = []
+let curr_question = ""
+
+$("#loader").hide()
+
+function get_query() {
+  age = $("#age").val()
+  return (age ? `I am ${age} years old. ` : "") + $("#question").val()
+}
+
+$("#question,#age").change(() => {
+  let query = get_query()
+  if (curr_question != query) {
+    $("#ask").text("ASK!").removeClass("purple").addClass("green")
+    curr_question = query
+  }
+})
+
+$("#ask").click(e => {
+  if (e.target.innerText == "ASK!") {
+    $("#advice").hide()
+    $("#loader").show()
+    fetch("https://miscserver.herokuapp.com/helpful", {
+      method: "POST",
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({question: curr_question})
+    }).then(data => data.json().then(jdata => {
+      advice = jdata
+      $("#loader").hide()
+      $("#advice").text(advice.shift()).show()
+      $(e.target).text("Ask Again!").removeClass("green").addClass("purple")
+    })).catch(console.log)
+  } else if (advice.length > 0) {
+    $("#advice").text(advice.shift())
+  }
+})
